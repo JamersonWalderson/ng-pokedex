914e2b86b43ade4f6233b7d0eaa46c83acb2c1d7
jamersonwalderson <jamersonwalderson@gmail.com>
2025-10-30 14:11:11 -0300

    feat(get): consumindo endpoint de pokedex

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pokemon } from './services/pokemon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ng-pokedex');
+  private pokemonService = inject(Pokemon);
+  protected data = signal<any>(null);
+  
+  constructor() {
+    this.pokemonService.all().subscribe((result) => {
+      this.data.set(result);
+    });
+  }
 }
--- src/app/services/pokemon.ts
+++ src/app/services/pokemon.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Pokemon {
+  private http = inject(HttpClient)
   
+  all(): Observable<any> {
+    return this.http.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/refs/heads/master/pokedex.json');
+  }
 }
